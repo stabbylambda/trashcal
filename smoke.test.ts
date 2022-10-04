2e41048248f902796616e38e4fa2a12a0cceb260
David Stone <david@stabbylambda.com>
2022-10-04 10:07:27 -0700

    parameterize the smoke test

--- smoke.test.ts
+++ smoke.test.ts
@@ -1,11 +1,14 @@
 import { describe, it, expect } from "vitest";
 import fetch from "cross-fetch";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const baseUrl = "https://trashcal.stabbylambda.com";
+const id = process.env["TRASHCAL_ID"] ?? "a4Ot0000001E8i4EAC";
 
 describe("post deploy", () => {
   it("works with a basic calendar", async () => {
-    let response = await fetch(`${baseUrl}/a4Ot0000001E8i4EAC`);
+    let response = await fetch(`${baseUrl}/${id}`);
     let body = await response.text();
     console.log(body);
 
@@ -17,7 +20,7 @@ describe("post deploy", () => {
   });
 
   it("works with a json calendar", async () => {
-    let response = await fetch(`${baseUrl}/a4Ot0000001E8i4EAC`, {
+    let response = await fetch(`${baseUrl}/${id}`, {
       headers: {
         accept: "application/json",
       },
@@ -26,8 +29,7 @@ describe("post deploy", () => {
     console.log(body);
 
     expect(response.headers.get("content-type")).toBe("application/json");
-    expect(body.id).toBe("a4Ot0000001E8i4EAC");
-    expect(body.address).toBe("1234 AGATE ST, San Diego, CA 92109");
+    expect(body.id).toBe(id);
     expect(body.pickups.length).toBeGreaterThan(0);
   });
 });
