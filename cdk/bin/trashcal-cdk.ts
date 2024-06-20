1b0ad0790df583346a9d28f547fa0a27835312c3
David Stone <david@stabbylambda.com>
2024-06-19 17:05:08 -0700

    debug action

--- cdk/bin/trashcal-cdk.ts
+++ cdk/bin/trashcal-cdk.ts
@@ -2,6 +2,12 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { TrashcalCdkStack } from "../lib/trashcal-cdk-stack";
+import { checkInstalledTarget } from "@cdklabs/aws-lambda-rust/lib/util";
+
+console.log(
+  "Target installed?",
+  checkInstalledTarget("aarch64-unknown-linux-gnu")
+);
 
 const app = new cdk.App();
 new TrashcalCdkStack(app, "TrashcalCdkStack", {});
