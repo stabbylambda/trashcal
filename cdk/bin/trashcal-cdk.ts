93ac8557d40ef5252a1c1da56ac31dd31e282075
David Stone <david@stabbylambda.com>
2024-06-19 17:14:27 -0700

    Debug action

--- cdk/bin/trashcal-cdk.ts
+++ cdk/bin/trashcal-cdk.ts
@@ -2,11 +2,6 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { TrashcalCdkStack } from "../lib/trashcal-cdk-stack";
-import { spawnSync } from "child_process";
-
-const proc = spawnSync("rustup", ["target", "list", "--installed"]);
-console.log(proc);
-console.log(proc.stdout.toString());
 
 const app = new cdk.App();
 new TrashcalCdkStack(app, "TrashcalCdkStack", {});
